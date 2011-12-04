284b207b8df8e8de377d4887b66bd7b2c4403fa0
Marcus Wanner <marcus@wanners.net>
2011-12-04 01:32:46 -0500

    Small cleanups in common.c's readblock()

--- common.c
+++ common.c
@@ -6,12 +6,8 @@ int readblock(long blocksize, char* buf){
 	//unfilled blocks (the last one, usually) are ignored
 	//returns 0 on a successfully filled block
 	int i=0;
-	char tmp=0;
 	for (i=0; i<blocksize; i++) {
-		//printf("%d\n", i);
-		if ((buf[i] = getchar()) == EOF) {
-			return(1);
-		}
+		if ((buf[i] = getchar()) == EOF) return(1);
 	}
 	return(0);
 }
