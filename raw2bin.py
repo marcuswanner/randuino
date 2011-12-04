07d60a66bbebeb1074abaac6bf907d5fe6edada7
Marcus Wanner <marcus@wanners.net>
2011-12-04 02:57:26 -0500

    Fixing major unicode bug in raw2bin.py

--- raw2bin.py
+++ raw2bin.py
@@ -2,19 +2,17 @@
 
 import sys
 
-stdin = sys.stdin
-stdout = sys.stdout
+sys.stdout = sys.stdout.detach()
 partbit = 0
 numbits = 0
 
 while 1:
-	line = stdin.readline()
+	line = sys.stdin.readline()
 	if not line: break
 	bit_in = int(line) % 2
-	#stdout.write(str(bit_in))
 	partbit = (partbit << 1) + bit_in
 	numbits = (numbits + 1) % 8
 	if numbits == 0:
-		sys.stdout.write(chr(partbit))
-		#print(partbit)
+		#this weird line stops stdout from unicoding the bytes
+		sys.stdout.write(bytes(chr(partbit), "latin-1"))
 		partbit = 0 
