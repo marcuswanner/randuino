afbf7428dc7235b0439fd74c22bba6117438fc37
Marcus Wanner <marcus@wanners.net>
2011-12-11 03:37:03 -0500

    Expanding server.py to handle complex pipes more easily
    Still to come: 	on-the-fly pipe topography changes
    		hopefully a better way to pass errors up and down the pipe

--- server.py
+++ server.py
@@ -5,17 +5,55 @@ from threading import Thread, Lock
 from time import sleep
 
 #basically an application-wide buffer size
+#this many bytes are handled at a time
+#up to this many bytes may be dropped when using tap()
+#and directing it to not copy the data (the default)
 BLOCKSIZE=128
 
-def tostr(bytearray): return
-tostr = lambda ba: str(ba, 'latin-1')
+
+tostr = lambda byte: str(byte, 'latin-1')
 tobytes = lambda string: bytes(string, 'latin-1')
+def totype(indata, targettype):
+    if type(indata) == targettype: return indata
+    if targettype == str:       return tostr(indata)
+    elif targettype == bytes:   return tobytes(indata)
 
 def run(command):
     "takes command and arguments in a list"
     "returns stdout and stdin file objects"
     p = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    return(p.stdout, p.stdin)
+    return(p.stdout, p.stdin, p)
+
+
+class StreamNode():
+    "A running command which is a piece of the data pipe"
+    def __init__(self, command, types):
+        self.stdout, self.stdin, self.p = run(command)
+        self.outtype, self.intype = types
+    
+    def stop():
+        self.p.kill()
+
+class EndNode():
+    "A file which is only written to and forms the end of a pipe"
+    def __init__(self, infile, datatype=str):
+        self.stdout, self.stdin = None, infile
+        self.outtype, self.intype = None, datatype 
+        self.p = None
+    
+    def stop():
+        pass
+
+class SourceNode():
+    "A file which is only read from and forms the beginning of the pipe"
+    def __init__(self, outfile, datatype=str):
+        self.stdout, self.stdin = outfile, None
+        self.outtype, self.intype = datatype, None
+        self.p = None
+    
+    def stop():
+        pass
+
 
 class SplitThread(Thread):
     "grabs input from an infile and duplicates it across one or more outs"
@@ -25,27 +63,36 @@ class SplitThread(Thread):
         self.setDaemon(True)
         self.infile, self.outfiles = infile, outfiles
         self.tapreqs = Queue()
-        self.tapblocks = Queue()
-        self.tapwaiting = Lock()
 
     def tap(self, n, copy=False):
         "grab n bytes from the running pipe"
         "if copy is False, the bytes returned (plus enough to make an even"
         "block) will be removed from the pipe and never seen by outfiles"
-        "returns a STRING!"
+        "returns bytes"
+        "this method SHOULD be threadsafe"
         nblocks = n//BLOCKSIZE+1
         blocks = []
         for bid in range(nblocks):
             q = Queue()
             self.tapreqs.put([copy, q])
             blocks.append(q.get())
-        return ''.join([tostr(b) for b in blocks])[:n]
+        return b''.join([tobytes(b) for b in blocks])[:n]
     
     def run(self):
         while 1:
-            sleep(0.2)
-            block = self.infile.read(BLOCKSIZE)
-            if len(block) < BLOCKSIZE:
+            #FIXME: when erroring out, we really need to identify who we're talking about
+            try:
+                block = self.infile.read(BLOCKSIZE)
+                if len(block) < BLOCKSIZE:
+                    raise Exception("Block not filled")
+            except:
+                #what do we do now? the command exited, probably
+                #in any case, we can't go on
+                #TODO: investigate what happens to downstream stuff
+                print("SplitThread's infile died...we're leaving now")
+                print(sys.exc_info())
+                for out in self.outfiles:
+                    out[0].close()
                 break
             if self.tapreqs.qsize():
                 copy, cbq = self.tapreqs.get()
@@ -53,22 +100,34 @@ class SplitThread(Thread):
                 if not copy:
                     continue
             for out in self.outfiles:
-                out.write(block)
-                out.flush()
+                try:
+                    out[0].write(totype(block, out[1]))
+                    out[0].flush()
+                except:
+                    print("Removing oufile from SplitThread():")
+                    print(sys.exc_info())
+                    self.outfiles.remove(out)
+
+def linknodes(innode, outnodes):
+    infile = innode.stdout
+    outfiles = []
+    for node in outnodes:
+        outfiles.append([node.stdin, node.intype])
+    splitter = SplitThread()
+    splitter.init(infile, outfiles)
+    splitter.start()
+    return splitter 
+
 
 if __name__ == "__main__":
-#    dumpout, dumpin = run('./dump.py')
-    dumpout, dumpin = run(['cat','testfile'])
-    hexout, hexin = run('hexdump')
-    testfileout = open('testfileout', 'wb')
-    bitbucket = open('/dev/null', 'w')
-    dump = SplitThread()
-    dump.init(dumpout, [testfileout])
-    dump.start()
+    #dumpn = StreamNode(['./dump.py', str(BLOCKSIZE)], [str, None])
+    dumpn = SourceNode(open('server.py', 'r'))
+    hexn = StreamNode(['hexdump', '-C'], [bytes, bytes])
+    printn = EndNode(sys.stdout)
+    dumps = linknodes(dumpn, [hexn])
+    hexs = linknodes(hexn, [printn])
     while 1:
-        sleep(1)
-        print(dump.tap(200, False))
-        sleep(1)
-        print(dump.tap(200, True))
-
+        i = dumps.tap(100, False)
+        #this makes a mess
+        print(i)
 
