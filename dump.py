2fcb4919ce4d2f3548353ca01da55159717ee886
Marcus Wanner <marcus@wanners.net>
2011-12-11 03:35:41 -0500

    Added blocksize parameter to dump.py

--- dump.py
+++ dump.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python2
 import serial, sys
 
+try:
+    chunksize = int(sys.argv[1])
+except IndexError:
+    chunksize = 4096
+
 stdout = sys.stdout
 
 s = serial.Serial('/dev/ttyUSB0', 19200)
 
 while 1:
-	buf = s.read(4096)
+	buf = s.read(chunksize)
 	if not buf:
 		break
 	stdout.write(buf)
+	stdout.flush()
